8ca26e19d045414c9530926eaf7a07580465055d
Asupan <jraka1703@gmail.com>
2026-05-05 20:26:09 +0700

    Update videos.js

--- api/videos.js
+++ api/videos.js
@@ -1,17 +1,31 @@
 export default async function handler(req, res) {
   const API_KEY = process.env.VIDARA_API_KEY;
 
+  let page = 1;
+  let allVideos = [];
+  let hasMore = true;
+
   try {
-    const response = await fetch(
-      `https://api.vidara.so/v1/video/list?api_key=${API_KEY}&limit=20`
-    );
+    while (hasMore && page <= 20) { // batas aman (hindari overload)
+      const response = await fetch(
+        `https://api.vidara.so/v1/video/list?api_key=${API_KEY}&page=${page}&limit=100`
+      );
 
-    const data = await response.json();
+      const data = await response.json();
+      const videos = data?.result?.videos || [];
 
-    // cache biar ringan
-    res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate");
+      allVideos.push(...videos);
 
-    res.status(200).json(data.result.videos);
+      if (videos.length < 100) {
+        hasMore = false;
+      }
+
+      page++;
+    }
+
+    res.setHeader("Cache-Control", "s-maxage=120, stale-while-revalidate");
+
+    res.status(200).json(allVideos);
   } catch (err) {
     res.status(500).json({ error: "Gagal ambil data" });
   }
